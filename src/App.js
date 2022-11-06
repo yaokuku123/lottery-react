ab24e1af88233569fe65cc23504a3e1ec6fe23eb
yaokuku123 <yaoqijun@outlook.com>
2022-11-06 21:01:16 +0800

    app中调用合约manager方法

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { Component } from 'react'
 let contractInstance = require('./eth/lotteryInstance')
 
-function App() {
-  return (
-     <p>Hello World</p>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      manager:'',
+      winner:''
+    }
+  }
+
+  async componentWillMount() {
+    let manager = await contractInstance.methods.manager().call() 
+    console.log(manager)
+    this.setState({
+      manager:manager
+    })
+  }
+
+  render(){
+      return (
+        <div>
+          <h2>App</h2>
+          <p>manager: {this.state.manager}</p>
+        </div>
+      )
+  }
 }
 
 export default App;
--- src/eth/lotteryInstance.js
+++ src/eth/lotteryInstance.js
@@ -1,98 +1,10 @@
-let web3 = require('../utils/initWeb3')
-let abi = [
-    {
-      constant: true,
-      inputs: [],
-      name: 'getBalance',
-      outputs: [ [Object] ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'round',
-      outputs: [ [Object] ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'manager',
-      outputs: [ [Object] ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'drawPrize',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'refund',
-      outputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'getPlayers',
-      outputs: [ [Object] ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: false,
-      inputs: [],
-      name: 'play',
-      outputs: [],
-      payable: true,
-      stateMutability: 'payable',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: 'winner',
-      outputs: [ [Object] ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      constant: true,
-      inputs: [ [Object] ],
-      name: 'players',
-      outputs: [ [Object] ],
-      payable: false,
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      inputs: [],
-      payable: false,
-      stateMutability: 'nonpayable',
-      type: 'constructor'
-    }
-  ]
+// let web3 = require('../utils/initWeb3')
+let Web3 = require('web3')
+let web3 = new Web3('http://localhost:8545')
+let abi = [{"constant":true,"inputs":[],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"round","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"drawPrize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"refund","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"play","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"winner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
 
-let address = '0xF6C89219cCD6973e5CC6C272907C27C0afB3B5Ec'
+let address = '0x4302BFc528E5dB085a05445689e76532e4A469D7'
 
 let contractInstance = new web3.eth.Contract(abi,address)
-
-console.log(contractInstance.options.address)
 
 module.exports = contractInstance
